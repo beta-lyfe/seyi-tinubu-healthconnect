491912eae6bd1641c8a94df5cc16fde74858d8f8
Adophilus <uchenna19of@gmail.com>
2024-07-07 23:46:06 +0100

    chore: included vite plugin for automatic tsconfig paths detection

--- webapp/@beta-lyfe/webapp/vite.config.ts
+++ webapp/@beta-lyfe/webapp/vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
     TanStackRouterVite(),
     react(),
+    tsconfigPaths(),
   ],
   server: {
     proxy: {
