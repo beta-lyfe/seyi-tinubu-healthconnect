7c2e90a74f18b5817fad01344dd0d1f2247ae5ea
Adophilus <uchenna19of@gmail.com>
2024-10-07 14:17:47 +0100

    chore: setup typegen for the frontend

--- webapp/@beta-lyfe/webapp/src/lib/backend/index.ts
+++ webapp/@beta-lyfe/webapp/src/lib/backend/index.ts
@@ -1,10 +1,11 @@
-import { hc } from 'hono/client'
-import type { App } from '@beta-lyfe/backend'
-import api from './api'
+import createClient from "openapi-fetch";
+import type { App } from "@beta-lyfe/backend";
+import api from "./api";
+import type { paths } from "./api/schema";
 
-const client = hc<App>('/')
+const client = createClient<paths>({ baseUrl: "/" });
 
 export const backend = {
-  client,
-  api
-}
+	client,
+	api,
+};
