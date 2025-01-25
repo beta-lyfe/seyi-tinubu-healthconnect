000fab8079ac68d5d04f7a01d6edf1774c5c4ba0
Uchenna Ofoma <uchenna19of@gmail.com>
2025-01-25 20:20:44 +0100

    Fixed the deployment (#43)
    * refactor: updated Dockerfile to work only for the backend
    
    * refactor: moved Dockerfile into the backend folder
    
    * fix: added apt command to install postgres
    
    * fix: corrected issues with the psycopg lib
    
    * fix: corrected issue with port
    
    * feat: added a script to start the prod server
    
    * fix: forgot to commit changes to the dockerfile
    
    * fix: corrected issue with the dockerfile entrypoint
    
    * fix: added missing pydantic requirement to requirements.txt
    
    * fix: uncommented the collectstatic command
    
    * fix: updated dockerfile
    
    * chore: updated gitignore
    
    * fix: added env arguments for dockerfile
    
    * chore: updated .env.example
    
    * chore: updated requirements.txt
    
    * fix: updated requirements.txt
    
    * fix: added email envs to dockerfile
    
    * feat: worked on github action
    
    * fix: replaced placeholder for .env
    
    * fix: updated github action file
    
    * fix: updated github actions
    
    * fix: updated node version of the github action
    
    * fix: updated frontend
    
    * fix: added working dir in github action
    
    * fix: removed the frontend dir
    
    * fix: updated the pnpm lockfile
    
    * fix: added the commadn to load .env
    
    * fix: added some debug statements
    
    * fix: updated the github workflow
    
    * fix: updated the github workflow
    
    * fix: updated the packages
    
    * fix: updated pacakge.json
    
    * fix: worked on .env
    
    * fix: added command to build the ui
    
    * fix: updated build script
    
    * fix: added env variables to vercel action
    
    * feat: added github action

--- packages/@beta-lyfe/website/scripts/build.sh
+++ packages/@beta-lyfe/website/scripts/build.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-tsc -b && vite build && rm -rf .vercel/output && mkdir .vercel/output && cp assets/vercel-config.json .vercel/output/config.json && cp -r dist .vercel/output/static
+tsc -b && vite build && rm -rf .vercel/output && mkdir -p .vercel/output && cp assets/vercel-config.json .vercel/output/config.json && cp -r dist .vercel/output/static
