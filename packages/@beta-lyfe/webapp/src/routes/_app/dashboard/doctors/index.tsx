69370fcf024da3cd8ab6b0fc9b1e6569141aa588
Adophilus <uchenna19of@gmail.com>
2025-02-27 13:08:16 +0100

    chore: committing changes done on the docs and the doctors listing page

--- packages/@beta-lyfe/webapp/src/routes/_app/dashboard/doctors/index.tsx
+++ packages/@beta-lyfe/webapp/src/routes/_app/dashboard/doctors/index.tsx
@@ -1,7 +1,11 @@
 import { Typography } from '@beta-lyfe/ui/components/typography'
 import { createFileRoute } from '@tanstack/react-router'
 import { Section } from '../-components/section'
-import { $api } from '../../../../lib/backend'
+import { $api, client } from '../../../../lib/backend'
+import { Loader2Icon } from 'lucide-react'
+import { useInView } from 'react-intersection-observer'
+import { useInfiniteQuery } from '@tanstack/react-query'
+import { useEffect, useRef } from 'react'
 
 export const Route = createFileRoute('/_app/dashboard/doctors/')({
   component: DoctorsPage
@@ -15,35 +19,95 @@ const TopBar = () => (
   </Section>
 )
 
+const fetchDoctors = async ({ page }: { page: string }) => {
+  const res = await client.GET('/api/doctors/', {
+    // params: {
+    //   query: {
+    //     page
+    //   }
+    // }
+  })
+
+  if (res.error) {
+    throw new Error(res.error.message)
+  }
+
+  return res.data
+}
+
 const DoctorsListing = () => {
-  const { data, status } = $api.useQuery('get', '/api/doctors/')
+  // const { data, status } = $api.useQuery('get', '/api/doctors/')
+  const { ref, inView: isInView } = useInView({
+    /* Optional options */
+    threshold: 0,
+    rootMargin: '500px'
+  })
+
+  console.log(isInView)
+
+  const { data, status, isFetchingNextPage, hasNextPage, fetchNextPage } =
+    useInfiniteQuery({
+      initialPageParam: '1',
+      queryKey: ['doctors', 'get'],
+      getNextPageParam: (lastPage) => {
+        if (!lastPage.next) return undefined
+
+        const url = new URL(lastPage.next)
+        const nextPage = url.searchParams.get('page')
+        if (!nextPage) return undefined
+
+        return nextPage
+      },
+      queryFn: ({ pageParam }) => fetchDoctors({ page: pageParam })
+    })
+
+  useEffect(() => {
+    if (isInView && hasNextPage && !isFetchingNextPage) fetchNextPage()
+  }, [isInView, isFetchingNextPage, hasNextPage])
 
   if (status === 'error' || status === 'pending') return null
 
-  const doctors = data.results.data
+  const doctors = data.pages.reduce(
+    (acc, curr) => acc.concat(curr.results.data),
+    [] as Awaited<ReturnType<typeof fetchDoctors>>['results']['data']
+  )
 
   return (
-    <div className="divide-y">
-      {doctors.map((doctor) => (
-        <div className="px-6 flex items-center gap-4 py-3" key={doctor.id}>
-          <div className="size-10 bg-slate-300 rounded-full" />
-          <div>
-            <Typography.Paragraph>
-              {doctor.first_name} {doctor.last_name}
-            </Typography.Paragraph>
-            <Typography.Info>General practitioner</Typography.Info>
+    <div>
+      <div className="divide-y">
+        {doctors.map((doctor) => (
+          <div className="px-6 flex items-center gap-4 py-3" key={doctor.id}>
+            <div className="size-10 bg-slate-300 rounded-full">
+              {/*<img
+                src={doctor.profile_picture_url}
+                alt={`${doctor.first_name} ${doctor.last_name}`}
+              />*/}
+            </div>
+            <div>
+              <Typography.Paragraph>
+                {doctor.first_name} {doctor.last_name}
+              </Typography.Paragraph>
+              <Typography.Info>General practitioner</Typography.Info>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
+      <div className="flex justify-center p-6" ref={ref}>
+        {isFetchingNextPage && (
+          <Loader2Icon className="animate-spin size-5 stroke-primary" />
+        )}
+      </div>
     </div>
   )
 }
 
 function DoctorsPage() {
   return (
-    <>
+    <div className="flex flex-col h-dvh">
       <TopBar />
-      <DoctorsListing />
-    </>
+      <div className="grow overflow-scroll">
+        <DoctorsListing />
+      </div>
+    </div>
   )
 }
