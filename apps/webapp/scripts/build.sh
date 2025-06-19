35ca3c752c55cc2111993d5176db1f299fdaffd1
Uchenna Ofoma <uchenna19of@gmail.com>
2025-06-19 23:00:20 +0100

    fix: fix the urls for the webapp and website (#83)
    * chore: updated the config for the webapp
    
    * choer: removed the version from the vercel config
    
    * chore: updated the destination path in the vercel config
    
    * chore: updated the vercel config
    
    * chore: updated the destination path
    
    * chore: updated the vercel config
    
    * chore: updated the vercel config
    
    * chore: updated the vercel config
    
    * chore:updated the vercel config
    
    * chore: updated vercel config
    
    * chore: updated the config
    
    * chore: updated vercel config
    
    * chore: updated vercel config
    
    * chore: updated the vercel config for the website and the build scripts for the apps
    
    * chore: corrected indentation

--- apps/webapp/scripts/build.sh
+++ apps/webapp/scripts/build.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-# vite build && \
 nx vite:build && \
-    pnpm lint && \
+  pnpm lint && \
     rm -rf .vercel/output && \
-    mkdir -p .vercel/output && \
-    cp assets/vercel-config.json .vercel/output/config.json && \
-    cp -r dist .vercel/output/static
+      mkdir -p .vercel/output && \
+        cp assets/vercel-config.json .vercel/output/config.json && \
+          cp -r dist .vercel/output/static
--- apps/website/scripts/build.sh
+++ apps/website/scripts/build.sh
@@ -1,3 +1,8 @@
 #! /usr/bin/env bash
 
-tsc -b && vite build && rm -rf .vercel/output && mkdir -p .vercel/output && cp assets/vercel-config.json .vercel/output/config.json && cp -r dist .vercel/output/static
+nx vite:build && \
+  pnpm lint && \
+    rm -rf .vercel/output && \
+      mkdir -p .vercel/output && \
+        cp assets/vercel-config.json .vercel/output/config.json && \
+          cp -r dist .vercel/output/static
